17537a36657334e8ec9bff20eb8a9ab59f5e208c
Chaseoungjun <chas369@naver.com>
2023-04-28 22:28:35 +0900

    스프링 배치를 사용하기위한 기본설정

--- src/main/java/com/example/SpringBatchPracticeApplication.java
+++ src/main/java/com/example/SpringBatchPracticeApplication.java
@@ -1,10 +1,13 @@
 package com.example;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchPracticeApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchPracticeApplication.class, args);
