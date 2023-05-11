b56ce2330b1ab6b072c0e4af0a2750df6f6e1f88
Chaseoungjun <chas369@naver.com>
2023-05-11 23:49:08 +0900

    실수로 빠진 @Bean 어노테이션 추가

--- src/main/java/com/example/job/pass/AddPassesJobConfig.java
+++ src/main/java/com/example/job/pass/AddPassesJobConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
@@ -17,21 +18,18 @@ public class AddPassesJobConfig {       // 이용권 일괄지급
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final AddPassesTasklet addPassesTasklet;
-    private final EntityManagerFactory entityManagerFactory;
 
+    @Bean
     public Job addPassesJob(){
        return jobBuilderFactory.get("addPassesJob")
                 .start(addPassesStep())
                 .build();
     }
-
+    @Bean
     public Step addPassesStep(){
         return stepBuilderFactory.get("addPassesStep")
                 .tasklet(addPassesTasklet)
                 .build();
     }
-
-
-
 
 }
