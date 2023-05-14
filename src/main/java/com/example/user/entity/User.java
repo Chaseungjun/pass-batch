a3d4dd1168ef22b66461e14a0dc964a494d88bf5
Chaseoungjun <chas369@naver.com>
2023-05-14 23:43:06 +0900

    이해를 돕기 위한 주석 추가

--- src/main/java/com/example/user/entity/User.java
+++ src/main/java/com/example/user/entity/User.java
@@ -26,15 +26,22 @@ public class User {
     private String userId;
 
     private String userName;
+
     @Enumerated(EnumType.STRING)
     private UserStatus status;
     private String phone;
 
+    /*
+    메시지를 받는 사람의 식별자인 uuid가 들어가있음
+    String 데이터가 Json형태로 바뀌어서 Map에 세팅이 된다
+    meta 필드의 하위에 uuid가 저장되어있다
+     */
     @Type(type = "json")
     private Map<String, Object> meta;
-    // 여기서 meta는 json 타입이다. (여기에 들어가있는 uuid는 메시지 api에서 설정한 uuid이다)
 
-    public String getUuid() {
+
+
+    public String getUuid() {    // meta 필드에서 uuid 값을 가져오는 메서드
         String uuid = null;
         if (meta.containsKey("uuid")) {
             uuid = String.valueOf(meta.get("uuid"));
